9c58468b23533e91119054344873093366e68aa0
Evgeny <estarinsky@gmail.com>
2025-08-31 13:59:36 +0300

    Update settings.py
    Updated settings European date format

--- learn/settings.py
+++ learn/settings.py
@@ -86,17 +86,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'en-GB'
+TIME_ZONE = 'Asia/Nicosia'
 USE_I18N = True
-
 USE_TZ = True
-
+USE_THOUSAND_SEPARATOR = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
