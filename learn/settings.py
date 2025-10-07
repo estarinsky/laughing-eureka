6b9a1efcb20330d1b778397ae309ea9e565b56f9
Evgeny <estarinsky@gmail.com>
2025-10-07 18:16:20 +0300

    fix: Finalize production setup and fix admin styles

--- learn/settings.py
+++ learn/settings.py
@@ -97,8 +97,6 @@ USE_I18N = True
 USE_TZ = True
 USE_THOUSAND_SEPARATOR = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
